3595eda0d551d7fb9727bec025f8e4b512fcae79
Ivan Garcia Martinez <ivagarci@c2r11s1.42madrid.com>
2023-04-11 21:22:22 +0200

    sadfsd

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdlk99 <kdlk99@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ivagarci <ivagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:39:33 by kdlk99            #+#    #+#             */
-/*   Updated: 2023/04/04 12:43:09 by kdlk99           ###   ########.fr       */
+/*   Updated: 2023/04/11 20:20:23 by ivagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*get_next_line(int fd);
 
 int	main(void)
 {
-	int	file = open("hola.txt", O_RDONLY);
+	int	file = open("test.txt", O_RDONLY);
 	char *str;
 	int	res;
 
@@ -44,17 +44,16 @@ char	*get_next_line(int fd)
 	int		i;
 	int		file;
 
-	if (fd == 0)
+	if (fd < 0)
 		return (NULL);
 	file = read(fd, &aux, 1);
-	printf("%d", file);
 	c = malloc(file * sizeof(char));
 	i = 0;
 	c[i] = aux;
-	while (c[i] != '\n' && file != 0)
+	while (c[i] != '\n' && file)
 	{
 		i++;
-		file = read(fd, &c[i], 1);
+		file = read(fd, &c[i], 1);		
 	}
 	if (file == 0)
 		return (NULL);
